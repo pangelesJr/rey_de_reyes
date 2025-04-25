6f39bb8b1bd3b8ed7dc5093e536642e45f845711
Pablo Angeles <pangeles@pabisretail.com>
2025-04-24 19:00:10 -0600

    fix(layout): add type annotation for children to fix TS error during build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,30 @@
 'use client';
 
+import { ReactNode } from 'react';
 import { Box } from '@mui/material';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import WhatsAppButton from './components/WhatsAppButton';
 
+interface LayoutProps {
+  children: ReactNode;
+}
 
-export default function Layout({ children }) {
-
+export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="es">
       <body style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <Navbar/>
-        <Box sx={{ marginTop: { xs: '90px', sm: '160px' }, paddingBottom: { xs: '90px', sm: '120px' }, flex: 1 }}> {/* Ajusta el margen superior y permite que el main ocupe el espacio restante */}
+        <Navbar />
+        <Box
+          sx={{
+            marginTop: { xs: '90px', sm: '160px' },
+            paddingBottom: { xs: '90px', sm: '120px' },
+            flex: 1,
+          }}
+        >
           <main>{children}</main>
         </Box>
-        <Footer/>
+        <Footer />
         <WhatsAppButton />
       </body>
     </html>
