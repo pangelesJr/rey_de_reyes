92f2694a61ccd88920c410a26923c36cdb5aa3b0
Pablo Angeles <pangeles@pabisretail.com>
2026-02-03 12:33:12 -0600

    Add type number and string to price

--- src/interface/product.interface.ts
+++ src/interface/product.interface.ts
@@ -3,7 +3,7 @@ export interface IProduct {
   id: number;
   name: string;
   autor?: string;
-  price: string;
+  price: string | number;
   discount?: number;
   description: string;
   features?: Record<string, string | undefined>;
